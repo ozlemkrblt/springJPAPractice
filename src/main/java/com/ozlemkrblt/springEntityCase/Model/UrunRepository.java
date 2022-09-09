30f4ccf97d963bc726ed4c8607ceaf4dff1040b1
ozlemkrblt <ozlemkarabulut99@gmail.com>
2022-09-09 16:16:39 +0300

    -m "New model repositories added."

---
+++ src/main/java/com/ozlemkrblt/springEntityCase/Model/UrunRepository.java
@@ -0,0 +1,8 @@
+package com.ozlemkrblt.springEntityCase.Model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UrunRepository extends JpaRepository<Kullanici,Long> {
+
+}
+
---
+++ src/main/java/com/ozlemkrblt/springEntityCase/Model/UrunYorumRepository.java
@@ -0,0 +1,8 @@
+package com.ozlemkrblt.springEntityCase.Model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UrunYorumRepository extends JpaRepository<Kullanici,Long> {
+
+}
+
